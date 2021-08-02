ef0c87d4c3b530c46fdf551c7937cd319b375e40
PriyanshuKumarSinha <priyanshuk9066@gmail.com>
2021-08-02 12:43:47 +0530

    V.1.0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log(window.innerWidth)
+let menuIcon = document.querySelector('#menuIcon');
+let menuLinks = document.querySelector('.links');
+
+function ham(cls){
+    changeMenuSymbol(cls);
+
+}
+
+function changeMenuSymbol(cls){
+    if (cls === 'fas fa-bars') {
+        menuIcon.setAttribute('class','fas fa-times');
+        menuLinks.style.display = 'block';
+
+    }
+    else{
+        menuIcon.setAttribute('class','fas fa-bars')    
+        menuLinks.style.display = 'none';
+    }
+}
